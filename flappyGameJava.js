3b93958e5a470bc1502b13bc6ad5761f226b9b38
shahshilp <shilpshah98@gmail.com>
2017-08-21 23:40:33 -0400

    worked on flappy game

--- flappyGameJava.js
+++ flappyGameJava.js
@@ -1,38 +1,116 @@
+
 var canvas = document.getElementById('canvas');
 canvas.width = canvas.scrollWidth;
 canvas.height = canvas.scrollHeight;
 var ctx = canvas.getContext('2d');
 
-var w = 800;
-var h = 500;
 var loop;
-var obstacle = [];
+var formWidth = 500;
+var formHeight = 500;
+var test = 10;
+var ob1, ob2;
+var open1, open2;
+var paintOb2;
+var player;
+var relYPos;
+var g = 1;
+var up = false;
+var downSpeed = 5;
 
-$( document ).ready(function() {
-	'use strict';
-    init();
-});
-function createObstacle()
+function Obstacle(xVal, yVal)
 {
-	'use strict';
-	obstacle = [1,2,3];
+	this.x = xVal;
+	this.y = yVal;
 }
-function paint{
+function Opening()
+{
+	//Generate random number between 150 - 250
+	this.s = (Math.floor(Math.random()*151) + 150);
+
+	//Generage random number between 0 - 350
+	this.p = (Math.floor(Math.random()* 351));
+}
+function createPlayer(){
+	player = {x:200, y:251, w:30, h:30};
+}
+
+function paint() {
+	//Repaint background every time
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0, 0, formWidth, formHeight);
+
+	//Painting Obstacle One
+	paintCell(ob1.x, ob1.y, 20, 500, 'red');
+	//Painting Opening One
+	paintCell(ob1.x, open1.p, 20, open1.s, 'black');
+	ob1.x -= 3;
+
 	
-	for(i = 0; i < obstacle.length; i++)
+	if (ob1.x === 252)
 	{
-		paintCell(obstacle[i], obstacle[i], 10, 100, 'red');
+		//Creating obstacle and opening 2
+		ob2 = new Obstacle(525, 0);
+		open2 = new Opening();
+		paintOb2 = true;
 	}
+	if (ob1.x === -18)
+	{
+		ob1.x = 525;
+	}
+
+	if(paintOb2 == true)
+	{
+		//Paining obstacle and opening 2
+
+		paintCell(ob2.x, ob2.y, 20, 500, 'red');
+		paintCell(ob2.x, open2.p, 20, open2.s, 'black');
+		ob2.x -= 3;
+	}
+
+	//Painting player
+	paintCell(player.x, player.y, player.w, player.h, 'blue');
+	//Player gravity
+
+	player.y += downSpeed;
+
+	//Moving player up
+	if(up == true)
+	{
+		player.y -= 15;
+	}
+}
+
+window.onkeydown = function(e) {
+   var key = e.keyCode ? e.keyCode : e.which;
+
+   if (key == 38) 
+   {
+       up = true;
+   }
+}
+window.onkeyup = function(e) {
+   var key = e.keyCode ? e.keyCode : e.which;
+
+   if (key == 38) 
+   {
+       up = false;
+   }
 }
 
 function paintCell(x, y, w, h, color)
 {
-	'use strict';
 	ctx.fillStyle = color;
 	ctx.fillRect(x, y, w, h);
 	ctx.strokeStyle = 'white';
-	ctx.strokeRect(x*cell, y*cell, cell, cell);
+	ctx.strokeRect(x, y, w, h);
 }
-function init(){
-	loop = setInterval(paint, 100);
+
+function init() {
+	ob1 = new Obstacle(525, 0);
+	open1 = new Opening();
+	createPlayer();
+	clearInterval(loop);
+	loop = setInterval(paint, 50);
 }
+
+window.onload(init());
